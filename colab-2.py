9197b8257f943a18da821251b402eb79e80d3e97
Iasmin Angel <143419093+IasminAngel@users.noreply.github.com>
2024-10-31 12:22:57 +0000

    Olá

--- colab-2.py
+++ colab-2.py
@@ -3,3 +3,7 @@ import os
 os.system("cls || clear")
 
 print("Colaborador - 2")
+
+# Continuar código mais tarde, seu preguiçoso, vai trabalhar, marrapaz. Solicite dados pro usuário aê
+
+noem = input("Digite seu nome:")
