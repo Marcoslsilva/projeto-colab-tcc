44132765fba7a3e0c84c80639f08000496917332
Marcoslsilva <112900211+Marcoslsilva@users.noreply.github.com>
2024-10-31 12:33:07 +0000

    Agora vai, resolvemos o conflito

--- colab-2.py
+++ colab-2.py
@@ -7,3 +7,5 @@ print("Colaborador - 2")
 # Continuar código mais tarde, seu preguiçoso, vai trabalhar, marrapaz. Solicite dados pro usuário aê
 
 noem = input("Digite seu nome:")
+#solicitando dados para o usuário
+idade = int(input("Digite sua idade: "))
