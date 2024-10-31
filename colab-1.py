810a9674ec4d4c90847ab124e38d67354b17d9e9
Marcoslsilva <112900211+Marcoslsilva@users.noreply.github.com>
2024-10-31 11:59:17 +0000

    Colaboracao inicial

---
+++ colab-1.py
@@ -0,0 +1,22 @@
+import os
+
+os.system("cls || clear")
+
+print("Colaborador Marcos")
+
+# comandos do git
+
+# Atualiza o repositórioatual com dados do repositório no Github
+# git pull
+
+# Adiciona novos dados para o commit
+# git add.
+
+#Versiona com os novos dados 
+#git commit -m "Título para o commit"
+
+#Atualiza trazendo commits do Github
+# git pull
+
+# Envia os commits para o Github
+#git push
