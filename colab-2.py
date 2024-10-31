428b1ce4502e940032f2a463bb719972d6698cb9
Iasmin Angel <143419093+IasminAngel@users.noreply.github.com>
2024-10-31 12:35:51 +0000

    Arrumando

--- colab-2.py
+++ colab-2.py
@@ -5,7 +5,8 @@ os.system("cls || clear")
 print("Colaborador - 2")
 
 # Continuar código mais tarde, seu preguiçoso, vai trabalhar, marrapaz. Solicite dados pro usuário aê
+# Solicitando dados para o usuário
 
-noem = input("Digite seu nome:")
-#solicitando dados para o usuário
+nome = input("Digite seu nome:")
+
 idade = int(input("Digite sua idade: "))
